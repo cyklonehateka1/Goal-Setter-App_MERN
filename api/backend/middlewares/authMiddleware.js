fb646c2fbea443c2fcaac1120dd86e9ec8a6710a
Emmanuel Hateka <emmanuelhateka.com>
2022-06-04 17:12:38 +0000

    jwt authorization in progress

---
+++ api/backend/middlewares/authMiddleware.js
@@ -0,0 +1,34 @@
+import jwt from "jsonwebtoken";
+import expressAsyncHandler from "express-async-handler";
+import userModel from "../models/userModel.js";
+
+const protect = expressAsyncHandler(async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      // Get token from header
+      token = req.headers.authorization.split(" ")[1];
+
+      // Verify token
+      const decoded = jwt.verify(token, process.env.JWT_SEC);
+
+      //   Get user from the token
+      req.user = await userModel.findById(decoded.id).select("-password");
+      next();
+    } catch (error) {
+      console.log(error);
+      res.status(401);
+      throw new Error("Not authorized");
+    }
+  }
+  if (!token) {
+    res.status(401);
+    throw new Error("No token");
+  }
+});
+
+export default protect;
